7c1b389bce1b1592200ea063bcdf43be38090e24
Francisco Javier Restrepo Castañeda <franjaresc@hotmail.com>
2022-03-16 17:01:32 -0500

    Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
-import { NavbarBrand, Navbar } from 'reactstrap';
+import { DISHES } from './shared/dishes';
+
 
 function App() {
+
+  const [dishes, setDishes] = useState(DISHES);
   return (
     <div className="App">
       <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Risorante Con Fusion</NavbarBrand>
-        </div>
+      <NavbarBrand href="/">Risorante Con Fusion</NavbarBrand>
+        
       </Navbar>
+      <Menu dishes={dishes}/>
     </div>
   );
 }
---
+++ src/components/MenuComponent.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+
+function Menu(props) {
+  const [selectedDish, setSelectedDish] = useState(null);
+  const onDishSelected = (dish) => {
+    setSelectedDish(dish);
+  };
+  const renderDish = (dish) => {
+    if (dish != null) {
+      return (
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      return <div></div>;
+    }
+  };
+  const menu = props.dishes.map((dish) => (
+    <div key={dish.id} className="col-12 col md-5 m-1">
+      <Card key={dish.id} onClick={() => onDishSelected(dish)}>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardImgOverlay>
+          <CardTitle>{dish.name}</CardTitle>
+        </CardImgOverlay>
+      </Card>
+    </div>
+  ));
+
+  return (
+    <div className="container">
+      <div className="row">
+          {menu}
+          </div>
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          {renderDish(selectedDish)}
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Menu;
